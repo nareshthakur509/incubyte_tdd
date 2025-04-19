55375ed3d666d0630acbf80663f880f90a5d301b
Naresh <naresh@Mediguru.io>
2025-04-19 17:58:10 +0530

    worked on sum method and test cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,45 @@
 class StringCalculator
-  def self.add(input)
-    return 0 if input.empty?
+  def add(string_numbers)
+    return 0 if string_numbers.strip.empty?
+
+    if string_numbers.start_with?("//")
+      delimiter_section, numbers = string_numbers.split("\n", 2)
+      custom_delimiter = delimiter_section[2..-1]
+    else
+      custom_delimiter = nil
+      numbers = string_numbers
+    end
+
+    delimiters = [",", "\n"]
+    if custom_delimiter
+      if custom_delimiter.start_with?("[") && custom_delimiter.end_with?("]")
+        custom_delimiter = custom_delimiter[1..-2]
+      end
+      delimiters << custom_delimiter
+    end
+
+    delimiters.each do |delim|
+      numbers = numbers.gsub(delim, ",")
+    end
+
+    numbers = numbers.split(",").map(&:to_i)
+    negatives = numbers.select { |n| n < 0 }
+    if negatives.any?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+    numbers.sum
   end
 end
+
+
+# calc = StringCalculator.new
+# puts calc.add("")
+# puts calc.add("1,2")
+# puts calc.add("1\n2,3")
+# puts calc.add("//;\n1;2")
+
+# begin
+#   puts calc.add("1,-2,3,-4")
+# rescue => e
+#   puts e.message
+# end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,9 +2,82 @@ require 'spec_helper'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
-  describe '.add' do
-    it 'returns 0 for an empty string' do
-      expect(StringCalculator.add("")).to eq(0)
+  let(:calculator) { StringCalculator.new }
+
+  describe "#add" do
+    context "when the input is an empty string" do
+      it "returns 0" do
+        input = ""
+        output = 0
+        expect(calculator.add(input)).to eq(output)
+      end
+    end
+
+    context "when the input contains one number" do
+      it "returns the number itself" do
+        input = "1"
+        output = 1
+        expect(calculator.add(input)).to eq(output)
+      end
+    end
+
+    context "when the input contains two numbers separated by a comma" do
+      it "returns the sum of the numbers" do
+        input = "1,5"
+        output = 6
+        expect(calculator.add(input)).to eq(output)
+      end
+    end
+
+    context "when the input contains numbers separated by newlines" do
+      it "returns the sum of the numbers" do
+        input = "1\n2,3"
+        output = 6
+        expect(calculator.add(input)).to eq(output)
+      end
+    end
+
+    context "when the input contains custom delimiters" do
+      it "returns the sum of the numbers using the custom delimiter" do
+        input = "//;\n1;2;3"
+        output = 6
+        expect(calculator.add(input)).to eq(output)
+      end
+
+      it "returns the sum of the numbers using another custom delimiter" do
+        input = "//[***]\n1***2***3"
+        output = 6
+        expect(calculator.add(input)).to eq(output)
+      end
+    end
+
+    context "when the input contains negative numbers" do
+      it "raises an error with negative numbers not allowed message" do
+        input = "1,-2,3,-4"
+        expect { calculator.add(input) }.to raise_error("negative numbers not allowed -2,-4")
+      end
+    end
+
+    context "when the input contains multiple numbers including zero" do
+      it "returns the sum of all the numbers" do
+        input = "0,1,2,3,4"
+        output = 10
+        expect(calculator.add(input)).to eq(output)
+      end
+    end
+
+    context "when the input contains custom delimiters and newlines" do
+      it "returns the sum of the numbers using multiple delimiters" do
+        input = "//[***]\n1***2***3,4\n5"
+        output = 15
+        expect(calculator.add(input)).to eq(output)
+      end
     end
   end
 end
+
+
+
+
+
+
